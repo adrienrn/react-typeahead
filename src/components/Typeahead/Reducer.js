e45bf4fca4b5d67b9cd93e87ea6888ad68c34153
Hussard <adrien.ricartnoblet@gmail.com>
2019-10-21 11:19:20 -0700

    Make the search/query debounced

---
+++ src/components/Typeahead/Reducer.js
@@ -0,0 +1,30 @@
+export const initialState = {
+  matches: [],
+  highlightedMatch: null,
+};
+
+export default function reducer(state, action)
+{
+  switch (action.type) {
+    case 'HIGHLIGHT_MATCH_DECREMENT':
+      return {
+        ...state,
+        highlightedMatch: Math.max(0, state.highlightedMatch - 1),
+      };
+    case 'HIGHLIGHT_MATCH_INCREMENT':
+      return {
+        ...state,
+        highlightedMatch: Math.min(state.matches.length - 1, state.highlightedMatch + 1),
+      };
+    case 'MATCHES_SET':
+      return {
+        matches: action.payload,
+        highlightedMatch: 0,
+      };
+
+    case 'MATCHES_RESET':
+      return initialState;
+    default:
+      return state;
+  }
+}
--- src/components/Typeahead/index.js
+++ src/components/Typeahead/index.js
@@ -1,5 +1,8 @@
-import React, { useEffect, useReducer, useState, } from 'react';
+import React, { useCallback, useEffect, useReducer, useState, } from 'react';
 import cx from 'classnames';
+import debounce from 'lodash.debounce';
+
+import reducer, { initialState, } from './Reducer.js';
 
 import s from './style.module.css';
 
@@ -8,44 +11,13 @@ export { createLocalDataSource } from './LocalDataSource.js';
 const CONTROL_KEYS = {
   37: false,
   38: {
-    type: 'HIGHLIGHT_MATCH_DECREMENT'
+    type: 'HIGHLIGHT_MATCH_DECREMENT',
   },
   39: false,
   40: {
-    type: 'HIGHLIGHT_MATCH_INCREMENT'
+    type: 'HIGHLIGHT_MATCH_INCREMENT',
   },
 };
-
-const initialState = {
-  matches: [],
-  highlightedMatch: null,
-};
-
-function highlightedReducer(state, action)
-{
-  switch (action.type) {
-    case 'HIGHLIGHT_MATCH_DECREMENT':
-      return {
-        ...state,
-        highlightedMatch: Math.max(0, state.highlightedMatch - 1),
-      };
-    case 'HIGHLIGHT_MATCH_INCREMENT':
-      return {
-        ...state,
-        highlightedMatch: Math.min(state.matches.length - 1, state.highlightedMatch + 1),
-      };
-    case 'MATCHES_SET':
-      return {
-        matches: action.payload,
-        highlightedMatch: 0,
-      };
-
-    case 'MATCHES_RESET':
-      return initialState;
-    default:
-      return state;
-  }
-}
 
 export default function Typeahead({
   dataSource,
@@ -55,12 +27,12 @@ export default function Typeahead({
 })
 {
   const [value, setValue] = useState('');
-  const [state, dispatchHighlightedMatch] = useReducer(highlightedReducer, initialState);
+  const [state, setState] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    //setFieldValue(name, value);
-
-    // Mock a query.
+  /**
+   * Building a debounced callback that will actually make the query when required.
+   */
+  const debouncedQuery = useCallback(debounce((value) => {
     if (value) {
       dataSource.query(value).then((matches) => {
         if (0 === matches.length) {
@@ -70,35 +42,40 @@ export default function Typeahead({
           return matches;
         }
 
-        dispatchHighlightedMatch({
+        setState({
           type: 'MATCHES_SET',
           payload: matches,
         });
       });
-
-      return;
     }
 
-    // Reset everything.;
-    dispatchHighlightedMatch({
+    // Reset everything.
+    setState({
       type: 'MATCHES_RESET',
     });
-  }, [dataSource, value]);
+  }, 350), [dataSource]);
+
+  /**
+   * Make the call to the data source after the value has changed.
+   */
+  useEffect(() => {
+    //setFieldValue(name, value);
+
+    debouncedQuery(value);
+  }, [debouncedQuery, value]);
 
   return (
     <div className={s['typeahead']}>
       <input
         name={name}
         placeholder={placeholder}
-        onChange={(event) => {
-          setValue(event.currentTarget.value);
-        }}
+        onChange={(event) => setValue(event.currentTarget.value)}
         onKeyDown={(event) => {
           if (undefined === CONTROL_KEYS[event.keyCode]) {
             return;
           }
 
-          dispatchHighlightedMatch(CONTROL_KEYS[event.keyCode]);
+          setState(CONTROL_KEYS[event.keyCode]);
         }}
         type="text"
         value={value}
